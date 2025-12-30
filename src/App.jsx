aef3d0211a0977245a621731cd5b78238657ffce
sanuriahangamage <sanuriahangamage@gmail.com>
2025-12-30 09:49:37 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Footer />
       </div>
     </Router>
   );
